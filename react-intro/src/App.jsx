f672962e302469b2e56d9b36d1fefe5e4cf21975
freeettt <122921308+freeettt@users.noreply.github.com>
2025-03-16 13:01:16 +0200

    Create App.jsx

---
+++ react-intro/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
+}
+
+export default App
